4b37fffd3d447aadec6e5219cf85cea22e7aa1d1
Matt Schultz <matt@envylabs.com>
2013-09-16 10:42:06 -0400

    DRY up driver object creation

--- test/spec/protractor/say_hello_spec.js
+++ test/spec/protractor/say_hello_spec.js
@@ -1,17 +1,15 @@
 var util = require('util');
 
 describe('Say Hello', function() {
-  var ptor;
+  ptor = protractor.getInstance();
 
   beforeEach(function() {
-    ptor = protractor.getInstance();
     ptor.get('#/');
     button = ptor.findElement(protractor.By.className('btn-say-hello'))
     button.click();
   });
 
   it('says hello', function() {
-    ptor = protractor.getInstance();
     message = ptor.findElement(protractor.By.className('message'))
     expect(message.getText()).toEqual('Hello!');
   });
